30accf1b035b1e478b6da3eec6bab5b6854c4cdf
NaamloosDT <NaamloosDT@gmail.com>
2018-11-20 13:55:24 +0100

    boop

--- src/GameData/MapManager.cs
+++ src/GameData/MapManager.cs
@@ -4,32 +4,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RpgGame.GameData
 {
     public class MapManager
     {
+        List<Map> Maps;
+
+        public MapManager()
+        {
+            Maps = new List<Map>();
+        }
+
+        public void LoadMaps()
+        {
+
+        }
     }
 
     public class Map
     {
+        [JsonProperty("mapid")]
+        public string MapId;
 
-    }
+        [JsonProperty("width")]
+        public int Width;
 
-    public class MapLayer
-    {
+        [JsonProperty("height")]
+        public int Height;
 
+        [JsonProperty("tilewidth")]
+        public int TileWidth;
+
+        [JsonProperty("tileheight")]
+        public int TileHeight;
+
+        [JsonProperty("maptiles")]
+        public List<MapTile> MapTiles;
+
+        [JsonProperty("mapcolissions")]
+        public List<MapColission> MapColissions;
+
+        [JsonProperty("mapevents")]
+        public List<MapEvent> MapEvents;
     }
 
     public class MapTile
     {
-        public bool Passable = true;
-        public IDrawable Sprite;
+        [JsonProperty("x")]
+        public int X;
 
-        public MapTile(IDrawable sprite, bool passable = true)
-        {
-            this.Sprite = sprite;
-            this.Passable = passable;
-        }
+        [JsonProperty("y")]
+        public int Y;
+
+        [JsonProperty("tileid")]
+        public string TileId;
+    }
+
+    public class MapColission
+    {
+        [JsonProperty("x")]
+        public int X;
+
+        [JsonProperty("x")]
+        public int Y;
+
+        [JsonProperty("colission")]
+        public ColissionLevel Colission;
+    }
+
+    public class MapEvent
+    {
+        [JsonProperty("x")]
+        public int X;
+
+        [JsonProperty("y")]
+        public int Y;
+
+        [JsonProperty("eventid")]
+        public string EventId;
+    }
+
+    public enum ColissionLevel
+    {
+        Pass,
+        NoPass,
+        Event
     }
 }
---
+++ src/GameData/TileManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgGame.GameData
+{
+    public class TileManager
+    {
+        Dictionary<string, IDrawable> Tiles;
+
+        public TileManager()
+        {
+            Tiles = new Dictionary<string, IDrawable>();
+        }
+
+        public void LoadTile(string id, IDrawable drawable)
+        {
+            Tiles.Add(id, drawable);
+        }
+    }
+}
